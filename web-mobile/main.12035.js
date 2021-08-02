890e0c4dfa08b3f7977be1a5c91a97dc674a0425
shijian6 <shijian6@game.sina.com>
2021-08-02 22:13:41 +0800

    all

--- web-mobile/main.12035.js
+++ web-mobile/main.12035.js
@@ -179,7 +179,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.7f568.js');
+        require('src/settings.780a7.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -187,7 +187,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.7f568.js');
+        require('src/settings.780a7.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
